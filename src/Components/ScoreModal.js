9c72ffa6fa6164eeac538a8fbb417e7b2704e0ad
kentaylor92 <ken.taylor.codes@gmail.com>
2021-04-14 16:31:48 -0400

    update winning score and winning message

--- src/Components/ScoreModal.js
+++ src/Components/ScoreModal.js
@@ -5,12 +5,12 @@ const ScoreModal = ({score, gameOver}) => {
     <div className="score">
       <p>Your score is: {score}</p>
       {!!gameOver && 
-        score < 20 ? 
+        score < 15 ? 
         <p className="loser">
           Try again! Reach the mystery high score to receive 20% off!
         </p>
         : 
-        <p className="winner">You win!  Your discount code is "KIM ROCKS"</p>
+        <p className="winner">You win! Take a screenshot to claim your prize at the event!</p>
       }
       </div>
 
