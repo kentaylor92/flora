3cdb4574632f2a6b8662325b05297ef58755bfb1
kentaylor92 <ken.taylor.codes@gmail.com>
2021-04-14 17:15:56 -0400

    modal desktop and mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Canvas from './Components/Canvas';
 import './App.scss';
 
 const App = () => {
+  const [close, setClose] = useState();
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setClose(false);
+    }, 1200)
+
+    return () => clearTimeout(timer)
+  }, [])
   return (
     <div className="App">
+      <div className={`instructions-modal ${close === false ? 'fade' : ''}`}
+      // style={{display: close === false ? 'block' : 'none'}}
+      >
+        <button onClick={e => {e.preventDefault(); setClose(true)}} >X</button>
+        <h3>Instructions</h3>
+        <p>Mobile users, simply swipe the direction you want your snake to go!</p>
+        <p>Desktop players can use keyboard arrows.</p>
+        <p>Keep trying until you reach the mystery score that will give you 20% off! </p>
+      </div>
       <img className="mobile-top-img" src="/assets/tambayan-mobile-top-flower-x2.png"></img>
       <img className="logo" src="/assets/logo-orange.png" alt="Tambayan by Paraluman Flora logo"></img>      
       <Canvas />
